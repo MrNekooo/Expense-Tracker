7edabaeb818f36568afd48b89b2b4269dfcefd99
Nekooo <mahdinekooo@gmail.com>
2025-08-07 08:57:20 +0330

    Changed CopyRight text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <p className="text-center">
+    <p className="text-center text-sm text-gray-500 dark:text-gray-400 mt-10">
         CopyRight &copy;Nekooo {new Date().getFullYear()}
     </p>
   )
